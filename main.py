4673500bff755629effdb8bff0847ceefe96722f
Bot1435 <113165247+Bot1435@users.noreply.github.com>
2022-09-09 09:01:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-class Zvire():
 
+def funkce1(a,b):
+    return a*b
 
-    def __init__(self, nazev, jmeno, vek, zuby):
-        self.__nazev = nazev
-        self.__jmeno = jmeno
-        self.vek = vek
-        self.zuby = zuby
+def funkce2(c,d):
+    return c*d
 
-    def get_jmeno(self):
-        return self.__jmeno
-
-    def get_nazev(self):
-        return self.__nazev
-
-    def printni(self):
-        print ("Zvire: {self.__nazev} jmenem {self.__jmeno} je:{self.vek} roku stare a ma akrualne {self.zuby} zubu.")
-
-
+ def funkce3(a,b):
+     return a,b
 
 
 if __name__ == '__main__':
-    zvire1 = Zvire("opice" , "pavel", 8, 20)
-    zvire2 = Zvire("medved", "Ota", 16, 28)
-    zvire1.printni()
-    zvire2.printni()
+
 
 
 
